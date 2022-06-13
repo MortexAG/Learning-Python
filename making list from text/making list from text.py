9726a18d36e49bd201d63edae29594b7d90b81f4
MortexAG <107272049+MortexAG@users.noreply.github.com>
2022-06-13 16:35:28 +0200

    Update making list from text.py

--- making list from text/making list from text.py
+++ making list from text/making list from text.py
@@ -4,7 +4,7 @@ from texttable import Texttable
 
 namesfile = open('names.txt')
 names = namesfile.read()
-agesfile = open('list maker/ages.txt')
+agesfile = open('ages.txt')
 ages = agesfile.read()
 professionsfile = open('professions.txt')
 professions = professionsfile.read()

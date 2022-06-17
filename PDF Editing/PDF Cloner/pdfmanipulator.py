2a660be8d55396a7b41516a19a8f2f75e2818a85
MortexAG <107272049+MortexAG@users.noreply.github.com>
2022-06-17 02:34:17 +0200

    Add files via upload

---
+++ PDF Editing/PDF Cloner/pdfmanipulator.py
@@ -0,0 +1,22 @@
+from itertools import count
+import PyPDF2
+#Enter The PDF Name
+print("Type the Name of The pdf file to Make a copy from")
+pdffirst = input()
+pdforiginal = (pdffirst + ".pdf")
+#Enter The Resulting pdf Name
+print("Enter The Resulting Pdf Name")
+pdfresult = input()
+pdfnew = (pdfresult + ".pdf")
+# Copy All Pages of the PDF
+pdffileobj = open(pdforiginal, "rb")
+pdfreader = PyPDF2.PdfFileReader(pdffileobj)
+pdfwrite = PyPDF2.PdfFileWriter()
+pagesnumber = pdfreader.numPages
+for pagesnum in range(pagesnumber):
+    pdfwrite.addPage(pdfreader.getPage(pagesnum))
+# Creat the New PDf
+pdfoutputfile = open(pdfnew, "wb")
+pdfwrite.write(pdfoutputfile)
+pdfoutputfile.close()
+pdffileobj.close()

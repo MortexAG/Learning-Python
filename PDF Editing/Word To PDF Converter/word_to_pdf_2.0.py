cac46da8dfe8c5b135102fd9964773dc2d4e14ee
MortexAG <107272049+MortexAG@users.noreply.github.com>
2022-07-17 12:44:47 +0200

    Add files via upload

---
+++ PDF Editing/Word To PDF Converter/word_to_pdf_2.0.py
@@ -0,0 +1,17 @@
+import docx2pdf
+import os
+from docx2pdf import convert
+print("Please Make Sure there's only one word file in the folder wordfile")
+input("Press Enter To Continue")
+import glob
+word_list = []
+os.mkdir("/wordfile")
+for filename in glob.glob('wordfile/*.docx'): #assuming docx
+    wordfile = (filename)
+    pdfname = "wordfile/" + filename + ".pdf"
+    print(pdfname)
+    pdf_pre = filename.replace('.docx','')
+    pdffile = (pdf_pre + ".pdf")
+#pdffile = ("wordfile/result.pdf")
+convert(wordfile, pdffile)
+convert("my_docx_folder/")

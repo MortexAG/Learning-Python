7ade7f8d35fb71286f98e26150ff3f6d53b85439
MortexAG <107272049+MortexAG@users.noreply.github.com>
2022-07-28 14:18:21 +0200

    Update clock.py
    add better UI and Week day with date times

--- Clock/clock.py
+++ Clock/clock.py
@@ -5,7 +5,11 @@ def clock():
   while(True):
       import time
       now = datetime.today()
+      day = datetime.today()
+      weekday = dat.strftime("%A")
+      thisday = day.strftime("%D/%M/%Y")
       timenow = now.strftime("%H:%M:%S")
-      print(timenow, end=("\r"))
+      print("Today Is:", weekday, thisday)
+      print("The Time Now is:"timenow, end=("\r"))
       time.sleep(1)
 clock()

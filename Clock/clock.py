4e107452854206860dce3e2ba9660cd086ea2fa3
MortexAG <107272049+MortexAG@users.noreply.github.com>
2022-07-28 14:07:27 +0200

    Create clock.py
    A Python Clock That Updates Every Second

---
+++ Clock/clock.py
@@ -0,0 +1,11 @@
+import datetime
+from datetime import datetime
+
+def clock():
+  while(True):
+      import time
+      now = datetime.today()
+      timenow = now.strftime("%H:%M:%S")
+      print(timenow, end=("\r")
+      time.sleep(1)
+ clock()
